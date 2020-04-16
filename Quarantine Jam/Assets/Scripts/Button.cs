a707f67dc5e47af477352db5eaf5eb2f3cf8edd2
Esteban SAINZ <esteban.sainz@etu.u-bordeaux.fr>
2020-04-16 12:28:41 +0200

    Basic Main Menu

---
+++ Quarantine Jam/Assets/Scripts/Button.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Button : MonoBehaviour
+{
+    [FormerlySerializedAs("_textMeshPro")] [SerializeField]
+    private TextMeshProUGUI textMeshPro;
+
+    // Start is called before the first frame update
+
+
+    private void Update()
+    {
+        textMeshPro.color = IsMouseOver() ? Color.black : new Color(0, 255, 0);
+    }
+
+    private static bool IsMouseOver()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+
+    // Update is called once per frame
+}
